05cdefa95231c71f1f1fc9f51c14f8831c1079cc
Analyce Ferreira <anallyce_@live.com>
2022-08-19 17:13:35 -0400

    Update techs and portfolio

---
+++ data/projects.js
@@ -0,0 +1,26 @@
+[
+    {
+        "title": "Plataform for ecommerce",
+        "image": "",
+        "description": "",
+        "techs": ["JavaScrip","EJS", "Node.JS", "Express", "Sequelize", "MySQL"],
+        "link":"https://github.com/analyceferreira/ecommerce-pi",
+        "type": "FullStack"
+    },
+    {
+        "title": "ICF landing page",
+        "image": "/icf-construtora-landing-page.png",
+        "description": "",
+        "techs": ["Vue.JS", "JavaScrip"],
+        "link":"https://github.com/analyceferreira/landing-page-icf-contrutora",
+        "type": "FrontEnd"
+    },
+    {
+        "title": "API Book Catalog",
+        "image": "/icf-construtora-landing-page.png",
+        "description": "",
+        "techs": ["Nest.JS", "TypeScript", "TypeORM", "SQLite"],
+        "link":"https://github.com/analyceferreira/book-catalog-api-challenge",
+        "type": "BackEnd"
+    }
+]
--- scrips/script.js
+++ scrips/script.js
@@ -1,9 +1,31 @@
 const portfolioElements = document.querySelectorAll('.portfolio-item');
 
-
 for (var item of portfolioElements) {
-    console.log(item);
+    if(item.classList.contains("ecommerce")){
     item.addEventListener('click', () => {
-        window.open("https://github.com/analyceferreira", "_blank");
-    })
+            console.log(item)
+            window.open("https://github.com/analyceferreira/ecommerce-pi", "_blank");
+        })
+    }
+
+    if(item.classList.contains("contrutora")){
+        item.addEventListener('click', () => {
+                console.log(item)
+                window.open("https://github.com/analyceferreira/landing-page-icf-contrutora", "_blank");
+            })
+    }
+
+    if(item.classList.contains("portfolio")){
+        item.addEventListener('click', () => {
+                console.log(item)
+                window.open("https://github.com/analyceferreira/site-portfolio-analyce-ferreira", "_blank");
+            })
+    }
+
+    if(item.classList.contains("bookAPI")){
+        item.addEventListener('click', () => {
+                console.log(item)
+                window.open("https://github.com/analyceferreira/book-catalog-api-challenge", "_blank");
+            })
+    }
 }
